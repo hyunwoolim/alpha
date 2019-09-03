548e1d26311cd79183603553104ce331cc8f6110
hyunwoolim <nice.hwlim@gmail.com>
2019-09-03 16:52:24 +0900

    초기세팅

---
+++ alpha-backend/src/main/java/com/max/alpha/AlphaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.max.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaBackendApplication.class, args);
+	}
+
+}
