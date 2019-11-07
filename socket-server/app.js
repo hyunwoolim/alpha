76339749164fa3c35950cd5fb099d9c11dcf4c60
MAX <hwlim@namee.co.kr>
2019-11-07 16:46:30 +0900

    채팅기능(개인채팅) 저장기능은 아직.

--- socket-server/app.js
+++ socket-server/app.js
@@ -2,14 +2,10 @@ const io = require('socket.io')(3000)
 console.log('1234')
 const chat = io.on('connection', (socket) => {
   socket.on('chat', (data) => {
-    console.log(data)
-    console.log('여기')
     io.to(data.room).emit('chat', data)
   })
   socket.on('joinRoom', (data) => {
-    console.log(data)
     socket.join(data, () => {
-      console.log('server join ' + data)
     })
   })
   /* chat.emit('a message', {
